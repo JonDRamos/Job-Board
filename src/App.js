ed1cba2245e484420cd4caa8839ec26db944dfa9
Day2DayDev <jonathandivinaramos@gmail.com>
2023-03-24 12:33:51 -0400

    creating react front end input forms

--- src/App.js
+++ src/App.js
@@ -4,7 +4,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-
+      <label>Name:</label>
+      <input type="text"/>
+      <label>City:</label>
+      <input type="text"/>
+      <label>Position:</label>
+      <input type="text"/>
+      <label>YOE:</label>
+      <input type="text"/>
     </div>
   );
 }
