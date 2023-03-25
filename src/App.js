25ba749d3fa293f5697a8f5a3dce8bb6581446d8
Day2DayDev <jonathandivinaramos@gmail.com>
2023-03-25 02:31:23 -0400

    able to connct frontend to db after many errors.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 
+
 import './App.css';
 //do not forget to to import useState. If not important, each line that uses useState will receive and error -"'useState' is not defined  no-undef".
 import {useState} from "react";
-
-
-
+//Step 6b. - importing Axios below
+import Axios from 'axios';
 
 //note that the body of the default syntax was deleted within the function App() { , everything after "<div className="App">"
 function App() {
@@ -13,6 +13,20 @@ function App() {
   const [city, setCity] = useState("");
   const [position, setPosition] = useState("");
   const [experience, setExperience] = useState(0); // note that useState is the initial states, therefore "" = emtpry string & "0" is initially 0 in input
+
+
+  //Step 6c. - creating a function to addUser (for when the input fields are entered)
+  const addUser = () => { //this function is added into the onClick in the Submit button below.
+    Axios.post("http://localhost:3001/create", {  //Axios allows us to make a HTTP request.  Create is the endpoint we have in 5c (server side). 
+      name:name,  //These are objects with KEY:VALUE pairs. Left side = back end variable in 5c - req.body.name. Right side = the variable in the front end (above - [name, setName]) in the useState statement.
+      city: city,
+      position: position,
+      experience:experience,   
+    }).then (() => {
+      console.log("success");
+    });
+  };
+
 
 //(Step 2b. below) Created input forms + labels
 //(Step 2g. below) Inputs were originally just - "<input type="text"/>" Adding onChange event to input - we are creating an event (event) and passing a function => (event.taget.value) is the value in the "targetted" input box, we then conenct it to setName (example). Do this for the rest of the table
@@ -40,8 +54,7 @@ function App() {
           setExperience(event.target.value);
           }} 
           />
-
-        <button> Submit</button>
+        <button onClick = {addUser}> Submit</button> 
         
       </div>
     </div>
